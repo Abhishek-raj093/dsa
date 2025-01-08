806d75bef64497dbd40faa756c7cb600652d0177
Abhishek-raj093 <ss0986825@gmail.com>
2025-01-08 23:04:36 +0530

    .

---
+++ Average.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class average {
+    public static void main(Strings[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = n;
+        int sum = 0;
+
+        while (m>0){
+            int num = sc.nextInt();
+            sum = sum + num;
+            m--;
+        }
+        System.out.println(sum/n);
+    }
+}
+
