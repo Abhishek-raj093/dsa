2469811357950f6b089d51f3aa6d05a398785b81
Abhishek-raj093 <ss0986825@gmail.com>
2025-02-13 22:07:25 +0530

    asad

--- sub.java
+++ sub.java
@@ -5,7 +5,7 @@ public class sub {
     try {
          int result = Math.subtractExact(x, y);
          System.out.println("Result: " + result);
-    } catch (ArithmeticException e) {
+    } catch(ArithmeticException e) {
         System.out.println("Overflow occurred during subtraction.");
     }
 }
