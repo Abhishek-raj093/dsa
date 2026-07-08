2b37834a29cbc52c2004905cf741d41d35459581
Abhishek-raj093 <ss0986825@gmail.com>
2026-07-08 17:33:22 +0530

    Remove Covered Intervals

---
+++ removeCoveredIntervals.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        int n = intervals.length; 
+        // 1. sort 
+        Arrays.sort(intervals, (a, b) -> {
+            if(a[0] == b[0]) return Integer.compare(b[1], a[1]); 
+            return Integer.compare(a[0], b[0]); 
+        }); 
+
+        int en = intervals[0][1]; 
+        int cnt = 0; 
+        // 2. count covered 
+        for(int i = 1; i < n; i++) {
+            int e = intervals[i][1]; 
+            if(e <= en) cnt++; 
+            else en = e; 
+        }
+
+        return n - cnt; 
+    }
+}
