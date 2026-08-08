73992e01e35b8d453b23718651386adfa359d3f9
Abhishek-raj093 <ss0986825@gmail.com>
2026-08-08 17:42:02 +0530

    Smallest Divisible Digit Product I

--- smallestNumber1.java
+++ smallestNumber1.java
@@ -1,3 +1,21 @@
-public class smallestNumber1 {
-    
+class Solution {
+    public int smallestNumber1(int n, int t) {
+
+        while (n < 101) {
+            int digits = n;
+            int product = 1;
+
+            while (digits > 0) {
+                product *= digits % 10;
+                digits /= 10;
+            }
+
+            if (product % t == 0)
+                return n;
+
+            n++;
+        }
+
+        return 0;
+    }
 }
