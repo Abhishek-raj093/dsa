1f016596f7e00a000383c8ec1054c86eee644345
Abhishek-raj093 <ss0986825@gmail.com>
2026-09-10 19:20:29 +0530

    Count Nodes Equal to Average of Subtree

--- averageOfSubtree.java
+++ averageOfSubtree.java
@@ -14,6 +14,26 @@
  * }
  */
 
-public class averageOfSubtree {
-    
+class Solution {
+    int res = 0;
+
+    public int averageOfSubtree(TreeNode root) {
+        postOrder(root);
+
+        return res;
+    }
+    public int[] postOrder(TreeNode node) {
+
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+        int[] left = postOrder(node.left);
+        int[] right = postOrder(node.right);
+        int sum = left[0] + right[0] + node.val;
+        int count = left[1] + right[1] + 1;
+        if (sum / count == node.val) {
+            res++;
+        }
+        return new int[]{sum, count};
+    }
 }
