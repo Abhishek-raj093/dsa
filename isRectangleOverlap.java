c410bca3ab45a36d2eff63e3e3e9d6b573e4b3bb
Abhishek-raj093 <ss0986825@gmail.com>
2026-09-14 08:44:23 +0530

    836. Rectangle Overlap

--- isRectangleOverlap.java
+++ isRectangleOverlap.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
         boolean xOverlap = Math.max(rec1[0], rec2[0]) < Math.min(rec1[2], rec2[2]);
